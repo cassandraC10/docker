b14bcb330d86523fed70f8729dcbf1a9a0c33bf3
Ejike Sandra <sandraejike101@gmail.com>
2023-01-04 07:37:01 +0100

    first commit

---
+++ src/test/java/com/cassandra/devopsdocker/DevOpsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassandra.devopsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
